72a3b68386bebc49a72ef26e44223e5422cf1369
iamraselmolla <raselmolla6336@gmail.com>
2025-07-31 13:41:47 +0600

    added the basic graphql project

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,5 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-
 // Define schema (typeDefs)
 const typeDefs = `#graphql
   type Book {
@@ -12,35 +11,30 @@ const typeDefs = `#graphql
     books: [Book]
   }
 `;
-
 // Sample data
 const books = [
-  {
-    title: "The Awakening",
-    author: "Kate Chopin",
-  },
-  {
-    title: "City of Glass",
-    author: "Paul Auster",
-  },
+    {
+        title: "The Awakening",
+        author: "Kate Chopin",
+    },
+    {
+        title: "City of Glass",
+        author: "Paul Auster",
+    },
 ];
-
 // Define resolvers
 const resolvers = {
-  Query: {
-    books: () => books,
-  },
+    Query: {
+        books: () => books,
+    },
 };
-
 // Create server with typeDefs and resolvers
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+    typeDefs,
+    resolvers,
 });
-
 // Start the server
 const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
+    listen: { port: 4000 },
 });
-
 console.log(`🚀 Server ready at: ${url}`);
