2eb857dc66ed3e2b6c262a288248a0512f0a1872
iamraselmolla <raselmolla6336@gmail.com>
2025-08-15 23:53:43 +0600

    edited

--- src/index.ts
+++ src/index.ts
@@ -43,4 +43,4 @@ const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
 });
 
-console.log(`🚀 Server ready at: ${url}`);
+console.log(`🚀 GraphQL Server ready at: ${url}`);
